095a34a458474f4e2c2434a6e32e99996e53aebb
ValerioScharizza <valerio.schiarizza@live.it>
2022-09-10 15:19:49 +0200

    Added images and created images array

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 console.log('JS OK!');
 
+const cards = [
+    { name: 'blank', img: '../img/blank.png' },
+    { name: 'blank', img: '../img/blank.png' },
+    { name: 'cheeseburger', img: '../img/cheeseburger.png' },
+    { name: 'cheeseburger', img: '../img/cheeseburger.png' },
+    { name: 'fries', img: '../img/fries.png' },
+    { name: 'fries', img: '../img/fries.png' },
+    { name: 'hotdog', img: '../img/hotdog.png' },
+    { name: 'hotdog', img: '../img/hotdog.png' },
+    { name: 'ice-cream', img: '../img/ice-cream.png' },
+    { name: 'ice-cream', img: '../img/ice-cream.png' },
+    { name: 'milkshake', img: '../img/milkshake.png' },
+    { name: 'milkshake', img: '../img/milkshake.png' },
+    { name: 'pizza', img: '../img/pizza.png' },
+    { name: 'pizza', img: '../img/pizza.png' },
+    { name: 'white', img: '../img/white.png' },
+    { name: 'white', img: '../img/white.png' }
+]
