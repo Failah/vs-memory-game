9377ab43c1e341946631f88abc04374abebe27ad
ValerioScharizza <valerio.schiarizza@live.it>
2022-09-10 17:01:42 +0200

    Created functions to flip cards and check matches

--- js/script.js
+++ js/script.js
@@ -19,8 +19,21 @@ const cards = [
     { name: 'white', img: 'img/white.png' }
 ]
 
+// arrays for images matching
+const cardsSelectedByUser = [];
+
+const cardsSelectedByUserFlip = [];
+
+const cardsMatched = [];
+
+
+// variables
+
 const grid = document.getElementById('grid');
 
+
+
+// generate a 4x4 images grid
 generateGrid();
 
 
@@ -34,8 +47,58 @@ function generateGrid() {
         card.setAttribute('data-id', i);
         card.classList.add('img-fluid', 'col-3');
 
-        // card.addEventListener('click', revealCard)
+        card.addEventListener('click', revealCard);
 
         grid.append(card);
     }
+}
+
+function revealCard() {
+    let cardFlip = this.getAttribute('data-id');
+
+    cardsSelectedByUser.push(cards[cardFlip].name);
+
+    cardsSelectedByUserFlip.push(cardFlip);
+
+    // this changes the clicked card with the corresponding in the cards array
+    this.setAttribute('src', cards[cardFlip].img);
+
+    if (cardsSelectedByUser.length === 2) {
+        setTimeout(checkMatch, 400);
+    }
+}
+
+function checkMatch() {
+    let selectedCards = document.querySelectorAll('img');
+
+    const firstSelect = cardsSelectedByUserFlip[0];
+    const secondSelect = cardsSelectedByUserFlip[1];
+
+    // checks if the selected cards matches and if so replaces them with white img
+    if (cardsSelectedByUser[0] === cardsSelectedByUser[1]) {
+
+        console.log('You found 2 matching cards!');
+
+        alert('You found 2 matching cards!');
+
+        selectedCards[firstSelect].setAttribute('src', 'img/white.png');
+        selectedCards[secondSelect].setAttribute('src', 'img/white.png');
+
+        // saves the matched card into a new array
+        cardsMatched.push(cardsSelectedByUser);
+    } else {
+        // if the cards do not match, this flips them back to original
+        selectedCards[firstSelect].setAttribute('src', 'img/blank.png');
+        selectedCards[secondSelect].setAttribute('src', 'img/blank.png');
+
+        console.log('You picked 2 non-matching cards! Try again!');
+
+        alert('You picked 2 non-matching cards! Try again!');
+    }
+
+    // we need to clear the storage array for choosen cards now
+    cardsSelectedByUser = [];
+    cardsSelectedByUserFlip = [];
+
+
 }
